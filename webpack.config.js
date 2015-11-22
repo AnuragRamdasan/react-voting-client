e5d07f4cc36fa73a4eb2ac13ffbde6908c13e9e6
Anurag Ramdasan <anuragr@live.com>
2015-11-22 23:22:08 +0530

    enable react hot loadings

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
 
+var webpack = require('webpack');
+
 module.exports = {
   entry: [
-    './src/index.js'
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './src/index.jsx'
   ],
   module: {
     loaders: [{
       test: /\.jsx?$/,
       exclude: /node_modules/,
-      loader: 'babel'
+      loader: 'react-hot!babel'
     }]
   },
   resolve: {
@@ -20,5 +24,8 @@ module.exports = {
   },
   devServer: {
     contentBase: './dist'
-  }
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
